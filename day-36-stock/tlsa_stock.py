347060bf3a1aaebeb2160bcef046ff21cfd46b88
Maruthi kalyan Reddy <maruthi2203@gmail.com>
2025-10-16 20:25:34 +0530

    Refactor stock data fetching code

--- day-36-stock/tlsa_stock.py
+++ day-36-stock/tlsa_stock.py
@@ -1,6 +1,6 @@
 import requests
 
-response =requests.get(url='https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=L3Y2Z7JWAKUUW5DF')
+response =requests.get(url='https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=')
 response.raise_for_status()
 data = response.json()
 close_9 = data['Time Series (Daily)']["2023-08-09"]['4. close']
@@ -11,7 +11,8 @@ percent = (abs(diff)/float(close_9))*100
 if percent > 2:
     print('get news')
 #news
-res = requests.get(url='https://newsapi.org/v2/everything?q=tesla&from=2023-08-8&sortBy=publishedAt&apiKey=56623c2d40a746759c1a935d55b15e8a')
+res = requests.get(url='https://newsapi.org/v2/everything?q=tesla&from=2023-08-8&sortBy=publishedAt&apiKey=')
 res.raise_for_status()
 new_data = res.json()
 article = new_data['articles'][3]
+
